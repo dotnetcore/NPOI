25b05c4c672badc71875d66ae978618e3b4c0175
Josh Comley <joshcomley@googlemail.com>
2016-10-11 20:24:39 +0100

    Added quick packaging batch script

---
+++ Code/pack.bat
@@ -0,0 +1,15 @@
+cd Npoi.Core
+dotnet pack --output ../packed
+cd ..
+cd Npoi.Core.Common
+dotnet pack --output ../packed
+cd ..
+cd Npoi.Core.Ooxml
+dotnet pack --output ../packed
+cd ..
+cd Npoi.Core.OpenXml4Net
+dotnet pack --output ../packed
+cd ..
+cd Npoi.Core.OpenXmlFormats
+dotnet pack --output ../packed
+cd ..
