9e520a7aa3f3ece4c7a03547176e33861ac11335
Josh Comley <joshcomley@googlemail.com>
2016-10-11 20:25:01 +0100

    Added sample project to create a new spreadsheet with some formatted cells

---
+++ Code/Npoi.Samples.CreateNewSpreadsheet/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Npoi.Core.HSSF.Record.Crypto;
+using Npoi.Core.OpenXmlFormats.Spreadsheet;
+using Npoi.Core.SS.UserModel;
+using Npoi.Core.XSSF.UserModel;
+
+namespace Npoi.Samples.CreateNewSpreadsheet
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var newFile = @"D:\temp\excel\newbook.core.xlsx";
+            using (var fs = new FileStream(newFile, FileMode.Create, FileAccess.Write))
+            {
+                IWorkbook wb = new XSSFWorkbook();
+                ISheet sheet = wb.CreateSheet("Sheet1");
+                //ICreationHelper cH = wb.GetCreationHelper();
+                var rowIndex = 0;
+                IRow row = sheet.CreateRow(rowIndex);
+                var cell = row.CreateCell(0);
+                var font = new XSSFFont();
+                font.IsBold = true;
+                cell.CellStyle.SetFont(font);
+                cell.SetCellValue("A very long piece of text that I want to auto-fit innit, yeah. Although if it gets really, really long it'll probably start messing up more.");
+                sheet.AutoSizeColumn(0);
+                rowIndex++;
+                wb.Write(fs);
+            }
+            Console.WriteLine("Done");
+        }
+    }
+}
