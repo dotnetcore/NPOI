deec9cb2de38fe8a3248e2843c87794126b64344
yangxiaodong <yangxiaodong1214@126.com>
2017-06-12 10:20:58 +0800

    Release version 1.1.4

--- tools/pack.bat
+++ tools/pack.bat
@@ -1,11 +1,11 @@
 cd ../src/Npoi.Core
-nuget pack -OutputDirectory ../../packed
-cd ..
-cd ../src/Npoi.Core.Ooxml
-nuget pack -OutputDirectory ../../packed
+nuget pack NPOI.Core.nuspec -OutputDirectory ../../packed
 cd ..
 cd ../src/Npoi.Core.OpenXml4Net
-nuget pack -OutputDirectory ../../packed
+nuget pack Npoi.Core.OpenXml4Net.nuspec -OutputDirectory ../../packed
 cd ..
 cd ../src/Npoi.Core.OpenXmlFormats
-nuget pack -OutputDirectory ../../packed
+nuget pack Npoi.Core.OpenXmlFormats.nuspec -OutputDirectory ../../packed
+cd ..
+cd ../src/Npoi.Core.Ooxml
+nuget pack NPOI.Core.Ooxml.nuspec -OutputDirectory ../../packed
