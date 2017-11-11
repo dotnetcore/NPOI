12ae23de8c4280dde6871c4eb75324846b7e931c
baiyunchen <baiyunchen@vip.qq.com>
2017-11-11 22:47:58 +0800

    fix bugs:issue32

---
+++ samples/Npoi.Samples.CreateNewSpreadsheet/Issue33.cs
@@ -0,0 +1,39 @@
+﻿using NPOI.XSSF.UserModel;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Npoi.Samples.CreateNewSpreadsheet
+{
+    class Issue33
+    {
+        public static void Run()
+        {
+            var issue33 = new Issue33();
+            issue33.WriteExcel();
+        }
+
+        public void WriteExcel()
+        {
+            string outputFileName = $@"D:\GitStorage\Npoi.Core\samples\Npoi.Samples.CreateNewSpreadsheet\template-{DateTime.Now.Ticks.ToString()}.xlsx";
+            using (var templateStream =new FileStream(@"D:\GitStorage\Npoi.Core\samples\Npoi.Samples.CreateNewSpreadsheet\template.xlsx", FileMode.Open, FileAccess.Read))
+            {
+                var excel = new XSSFWorkbook(templateStream);
+                var cell = excel.GetSheetAt(0).GetRow(2).GetCell(2);
+                cell.SetCellValue("Issue 33");
+
+                using (var outputStream = new FileStream(outputFileName, FileMode.Create, FileAccess.Write))
+                {
+                    excel.Write(outputStream);
+                }
+            }
+            using (var outputStream = File.OpenRead(outputFileName))
+            {
+                var excel = new XSSFWorkbook(outputStream);
+                var cell = excel.GetSheetAt(0).GetRow(2).GetCell(2);
+                Console.WriteLine(cell.ToString()); //here can console Issue 33
+            }
+        }
+    }
+}
