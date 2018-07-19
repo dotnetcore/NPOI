01264643b493d2dddd10f1faae2c7bdedff3bf0c
Savorboard <yangxiaodong1214@126.com>
2018-07-19 23:12:36 +0800

    Fix some compilation errors for unit tests

--- test/NPOI.OpenXml4Net/TestListParts.cs
+++ test/NPOI.OpenXml4Net/TestListParts.cs
@@ -33,7 +33,7 @@ namespace TestCases.OPC
 
         private Dictionary<PackagePartName, String> values;
 
-        [TestFixtureSetUp]
+        [OneTimeSetUp]
         public void SetUp()
         {
             values = new Dictionary<PackagePartName, String>();
