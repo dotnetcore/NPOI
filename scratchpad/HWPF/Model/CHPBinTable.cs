8ed51824eb9454a67ff7c0d570ccb4d5ee5d5602
lx <muteshell@163.com>
2018-09-22 16:04:45 +0800

    fix System.InvalidOperationException: Collection was modified; enumeration operation may not execute.        at System.Collections.Generic.List`1.Enumerator.MoveNextRare()        at System.Collections.Generic.List`1.Enumerator.MoveNext()        at NPOI.HWPF.Model.CHPBinTable.Rebuild(ComplexFileTable complexFileTable) in /Users/Private/git/tianzen-third lib/NPOI-netcore/scratchpad/HWPF/Model/CHPBinTable.cs:line 287        at NPOI.HWPF.HWPFDocument..ctor(DirectoryNode directory) in /Users/Private/git/tianzen-third lib/NPOI-netcore/scratchpad/HWPF/HWPFDocument.cs:line 267        at NPOI.HWPF.HWPFDocument..ctor(POIFSFileSystem pfilesystem) in /Users/Private/git/tianzen-third lib/NPOI-netcore/scratchpad/HWPF/HWPFDocument.cs:line 154        at NPOI.HWPF.HWPFDocument..ctor(Stream istream) in /Users/Private/git/tianzen-third lib/NPOI-netcore/scratchpad/HWPF/HWPFDocument.cs:line 141        at Toxy.Parsers.Word2003TextParser.Parse() in G:\code\toxy\ToxyFramework\Parsers\Word2003TextParser.cs:line 23

--- scratchpad/HWPF/Model/CHPBinTable.cs
+++ scratchpad/HWPF/Model/CHPBinTable.cs
@@ -22,6 +22,7 @@ using NPOI.HWPF.Model.IO;
 using System.IO;
 using NPOI.HWPF.SPRM;
 using System;
+using System.Linq;
 
 [assembly: System.Runtime.CompilerServices.InternalsVisibleTo("TestCases,PublicKey=002400000480000094000000060200000024000052534131000400000100010095ccd95af3b39d8bc20544d3f47fd24b53ebc5ccb693eaed116290629f8cd882c827ebd511ad59449224f0718d3f9d03b64945a6c8b6644266001b8c8426185330e3d96da70ae16d4acc21b8d4d480f1385c7e924273179375aa88f81380a72fb115712a313379d16aed4aa36208ee3b4a5dd785b06a07b2d868e3227f4495b5")]
 
@@ -283,7 +284,18 @@ namespace NPOI.HWPF.Model
                     _textRuns.Count, " elements)");
             start = DateTime.Now.Ticks;
 
+            /**
+             * System.InvalidOperationException: Collection was modified; enumeration operation may not execute.
+   at System.Collections.Generic.List`1.Enumerator.MoveNextRare()
+   at System.Collections.Generic.List`1.Enumerator.MoveNext()
+   at NPOI.HWPF.Model.CHPBinTable.Rebuild(ComplexFileTable complexFileTable) in /Users/Private/git/tianzen-third lib/NPOI-netcore/scratchpad/HWPF/Model/CHPBinTable.cs:line 287
+   at NPOI.HWPF.HWPFDocument..ctor(DirectoryNode directory) in /Users/Private/git/tianzen-third lib/NPOI-netcore/scratchpad/HWPF/HWPFDocument.cs:line 267
+   at NPOI.HWPF.HWPFDocument..ctor(POIFSFileSystem pfilesystem) in /Users/Private/git/tianzen-third lib/NPOI-netcore/scratchpad/HWPF/HWPFDocument.cs:line 154
+   at NPOI.HWPF.HWPFDocument..ctor(Stream istream) in /Users/Private/git/tianzen-third lib/NPOI-netcore/scratchpad/HWPF/HWPFDocument.cs:line 141
+   at Toxy.Parsers.Word2003TextParser.Parse() in G:\code\toxy\ToxyFramework\Parsers\Word2003TextParser.cs:line 23
+             */
             CHPX previous = null;
+            var copy = _textRuns.ToList();
             for (List<CHPX>.Enumerator iterator = _textRuns.GetEnumerator(); iterator
                     .MoveNext(); )
             {
